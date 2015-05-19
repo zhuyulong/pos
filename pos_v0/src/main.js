5342a9507de590d280f9cd1d2b9a071976219565
zhuyulong <356981944@qq.com>
2015-05-19 17:27:24 +0800

    add first  pos

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,4 +1,15 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
+        var result="***<没钱赚商店>购物清单***"+"\n";
+        var sum=0;
 
+        for(var i=0;i<inputs.length;i++){
+            result=result+"名称:"+inputs[i].name+"，数量："+inputs[i].count+inputs[i].unit+"，单价："
+            +inputs[i].price.toFixed(2)+"(元)"+"，小计："+(parseInt(inputs[i].price)*parseInt(inputs[i].count)).toFixed(2)+"(元)"+"\n";
+            sum+=(parseInt(inputs[i].price)*parseInt(inputs[i].count));
+
+        }
+        result=result+"---------------------"+"\n";
+        result+="总计:"+sum.toFixed(2)+"(元)";
+         console.log(result);
 }
