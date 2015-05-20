dcf86c395fd911608687de4aaefd6d4bbfc2564d
zhuyulong <356981944@qq.com>
2015-05-20 15:28:56 +0800

    add seven pos

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,14 +1,19 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
         var result="***<没钱赚商店>购物清单***"+"\n";
-        var sum=0;
+        var  number=0;
+        var  subtotal=0;//小计
+        var  sum=0; //总计
 
         for(var i=0;i<inputs.length;i++){
-            result=result+"名称:"+inputs[i].name+"，数量："+inputs[i].count+inputs[i].unit+"，单价："
-            +inputs[i].price.toFixed(2)+"(元)"+"，小计："+(parseInt(inputs[i].price)*parseInt(inputs[i].count)).toFixed(2)+"(元)"+"\n";
+            number=inputs[i].count+inputs[i].unit;
+            subtotal=(parseInt(inputs[i].price)*parseInt(inputs[i].count));
             sum+=(parseInt(inputs[i].price)*parseInt(inputs[i].count));
+            result+="名称:"+inputs[i].name+"，数量："+number+"，单价："
+            +inputs[i].price.toFixed(2)+"(元)"+"，小计："+subtotal+"(元)"+"\n";
 
         }
+
         result=result+"---------------------"+"\n";
         result+="总计:"+sum.toFixed(2)+"(元)";
          console.log(result);
