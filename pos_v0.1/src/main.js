2eb222a76d692890c0e25d6c67e0327105d6ac62
zhuyulong <356981944@qq.com>
2015-05-20 15:24:49 +0800

    add six pos

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -2,12 +2,14 @@
 function printInventory(inputs) {
     var result="***<没钱赚商店>购物清单***"+"\n";
     var array=[];
+
     for(var i=0;i<inputs.length;i++){
-        var  exist=false;
+         var  exist=false;
          for(var j=0;j<array.length;j++){
-              if(array[j].barcode==inputs[i].barcode){
+              if(array[j].barcode===inputs[i].barcode){
                    array[j].count++;
                    exist=true;
+                   break;
                       }
                 }
                     if(!exist){
@@ -15,14 +17,21 @@ function printInventory(inputs) {
                         array.push(inputs[i]);
                     }
             }
+
     var sum=0;
+    var  number=0;
+    var  subtotal=0;
+
     for(var i=0;i<array.length;i++){
-        result+="名称:"+array[i].name+"，数量："+array[i].count+array[i].unit+"，单价："
-        +array[i].price+".00(元)"+"，小计："+(parseInt(array[i].price)*parseInt(array[i].count))+".00(元)"+"\n";
+        number=array[i].count+array[i].unit;
+        subtotal=(parseInt(array[i].price)*parseInt(array[i].count));
         sum+=(parseInt(array[i].price)*parseInt(array[i].count));
+        result+="名称:"+array[i].name+"，数量："+number+"，单价："
+        +array[i].price.toFixed(2)+"(元)"+"，小计："+subtotal+"(元)"+"\n";
 
     }
+
     result=result+"---------------------"+"\n";
     result+="总计:"+sum.toFixed(2)+"(元)";
-      console.log(result);
+     console.log(result);
 }
